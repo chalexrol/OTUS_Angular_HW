471382790c878b92732c87d4df36a5db69ef843c
Alexandr Chukhvantsev <mob.pochta@gmail.com>
2020-02-11 17:15:44 +0400

    Memo App Part1

---
+++ src/app/word-sorter/word-sorter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-word-sorter',
+  templateUrl: './word-sorter.component.html',
+  styleUrls: ['./word-sorter.component.scss']
+})
+export class WordSorterComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+
+  }
+
+}
