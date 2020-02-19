6d74e6220cc952acdb8ab9b810c98e7ca3a6a24c
Alexandr Chukhvantsev <mob.pochta@gmail.com>
2020-02-19 11:28:02 +0400

    Add RxJS yandexTranslRequest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { SortablejsModule } from 'angular-sortablejs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WordSelectorComponent } from './word-selector/word-selector.component';
 import { WordSorterComponent } from './word-sorter/word-sorter.component';
+import { WordAddComponent } from './word-add/word-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordSelectorComponent,
-    WordSorterComponent
+    WordSorterComponent,
+    WordAddComponent
   ],
   imports: [
     BrowserModule,
